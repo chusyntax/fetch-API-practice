9a47652597304d0c85e55f1fce21a3dff68a4da2
chu-young <ttheko101@gmail.com>
2021-03-29 18:34:26 +0200

    added dynamic data

--- app.js
+++ app.js
@@ -8,17 +8,19 @@
 fetch('https://jsonplaceholder.typicode.com/photos')
 .then(res => res.json())
 .then(data => {
-    var jsonOutput = '';
+    let jsonOutput = '';
 
     //Then use a forEach loop since it is a JSON Array
 
     data.forEach(info => {
-        
-    });{
         jsonOutput +=`
-        
+        <ul>
+        <li>ID: ${info.id}</li>
+        <li>Title: ${info.title}</li>
+        <li><img src='${info.url}' width='70' height='70'> </li>
+        </ul>
         `
-    ;}
-output.innerHTML = jsonOutput;
+    });
+    output.innerHTML = jsonOutput;
 })
  }
